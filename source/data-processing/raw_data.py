1a47901b67508e2d6415ea6be14e09c799f9d1cd
jacktavitt <jacktavitt@gmail.com>
2019-01-23 12:18:46 -0800

    boto3 initial s3 access

--- source/data-processing/raw_data.py
+++ source/data-processing/raw_data.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # Idea here is to get this unruly bunch of miscreant data to fall in line and get on the same page.
 # Here is the ideal Schema for our ML tasks:
 # -------------------------------------
@@ -5,14 +7,32 @@
 # -------------------------------------
 # |   Session_ID
 # -------------------------------------
+# |   Sequence_Num
+# -------------------------------------
 # |   Key_ID
 # -------------------------------------
-# |   
+# |   Duration
 # -------------------------------------
-# |
-# -------------------------------------
-# |
-# -------------------------------------
-# |
-# -------------------------------------
-# |
+
+
+def UofBuffaloData(directory):
+    pass
+
+import argparse
+from pyspark import SparkContext, SparkConf
+import boto3
+import botocore
+
+BUCKET_NAME = 'u-of-buffalo' # replace with your bucket name
+KEY = '001001.txt' # replace with your object key
+
+s3 = boto3.resource('s3')
+resources = s3.get_available_subresources()
+print(f"Resources: {resources}\n")
+try:
+    s3.Bucket(BUCKET_NAME).download_file(KEY, 'my_local_image.jpg')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
