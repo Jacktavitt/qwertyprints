8a4c56b8aa9b8b2f8b8c577c0498da326537dbfd
jacktavitt <jacktavitt@gmail.com>
2019-01-24 00:15:50 -0800

    Got data from S3 into dataframe.

--- source/data-processing/raw_data.py
+++ source/data-processing/raw_data.py
@@ -18,21 +18,44 @@
 def UofBuffaloData(directory):
     pass
 
+
 import argparse
+import os
 from pyspark import SparkContext, SparkConf
-import boto3
-import botocore
+from pyspark.sql import SQLContext, SparkSession, Row, Column
+from pyspark.sql.types import *
 
-BUCKET_NAME = 'u-of-buffalo' # replace with your bucket name
-KEY = '001001.txt' # replace with your object key
+os.environ["PYSPARK_PYTHON"]="/usr/bin/python3"
+os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/bin/python3"
 
-s3 = boto3.resource('s3')
-resources = s3.get_available_subresources()
-print(f"Resources: {resources}")
-try:
-    s3.Bucket(BUCKET_NAME).download_file(KEY, 'my_local_image.jpg')
-except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == "404":
-        print("The object does not exist.")
-    else:
-        raise
+BUCKET_NAME = 'u-of-buffalo' 
+
+conf = SparkConf().setAppName("billy").setMaster("local[*]")
+spark = SparkSession.builder.appName("frank").getOrCreate()
+
+#schema didnt work for some reason... BECUASE I DID IT WRONG!
+# or did i? All values are 'None' when schema specified.
+# schema = StructType([
+#     StructField("key_id", StringType(), True),
+#     StructField("press_action", StringType(), True),
+#     StructField("action_time", IntegerType(), True)])
+
+# schema = StructType([StructField("key_id", StringType(), False),StructField("press_action", StringType(), False),StructField("action_time", IntegerType(), False), StructField("user_id", IntegerType(), False)])
+
+# df = spark.read.option("delimiter", " ").schema(schema).csv("s3a://u-of-buffalo/001001.txt")
+df = spark.read.option("delimiter", " ").csv("s3a://u-of-buffalo/001001.txt")
+# not ideal, but a hacky fix.
+df2 = df.withColumnRenamed('_c0','key_id').withColumnRenamed('_c1', 'key_action').withColumnRenamed('_c2','action_time')
+
+# go through and add columns
+
+sc = SparkContext(conf=conf)
+tf = sc.textFile("s3a://u-of-buffalo/001001.txt")
+
+# conf = SparkConf().setAppName("load_data").setMaster("local")
+# sc = SparkContext(conf=conf)
+# sqlContx = SQLContext(sc)
+
+# uob_bucket = s3.Bucket(BUCKET_NAME)
+# files_list = [f.key for f in uob_bucket.objects.all()]
+
