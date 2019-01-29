f63180f66636166454520eb1b891426f99a5e30b
jacktavitt <jacktavitt@gmail.com>
2019-01-28 16:33:09 -0800

    setting up sparkml gradient boosting

--- source/ml-hosting/serve_models.py
+++ source/ml-hosting/serve_models.py
@@ -8,24 +8,64 @@
 # and somethign else?
 
 # reference: https://spark.apache.org/docs/2.3.0/ml-classification-regression.html
+# reference: https://github.com/apache/spark/blob/master/examples/src/main/python/ml/gradient_boosted_tree_classifier_example.py
+"""
+Gradient Boosted Tree Classifier Example.
+"""
+from __future__ import print_function
 
-import json
-from pyspark.sql import SparkSession
+# $example on$
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+# $example off$
+from pyspark.sql import SparkSession
 
-def main():
-    """ 
-    Summary line. 
-  
-    Extended description of function. 
-  
-    Parameters: 
-    arg1 (int): Description of arg1 
-  
-    Returns: 
-    int: Description of return value 
-  
-    """
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("GradientBoostedTreeClassifierExample")\
+        .getOrCreate()
+
+    # $example on$
+    # Load and parse the data file, converting it to a DataFrame.
+    data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+
+    # Index labels, adding metadata to the label column.
+    # Fit on whole dataset to include all labels in index.
+    labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
+    # Automatically identify categorical features, and index them.
+    # Set maxCategories so features with > 4 distinct values are treated as continuous.
+    featureIndexer =\
+        VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = data.randomSplit([0.7, 0.3])
+
+    # Train a GBT model.
+    gbt = GBTClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", maxIter=10)
+
+    # Chain indexers and GBT in a Pipeline
+    pipeline = Pipeline(stages=[labelIndexer, featureIndexer, gbt])
+
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(trainingData)
+
+    # Make predictions.
+    predictions = model.transform(testData)
+
+    # Select example rows to display.
+    predictions.select("prediction", "indexedLabel", "features").show(5)
+
+    # Select (prediction, true label) and compute test error
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+    print("Test Error = %g" % (1.0 - accuracy))
+
+    gbtModel = model.stages[2]
+    print(gbtModel)  # summary only
+    # $example off$
+
+spark.stop()
