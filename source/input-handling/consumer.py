435ee98303ad32f490ceff635f190d9cac18b81d
jacktavitt <jacktavitt@gmail.com>
2019-01-31 15:04:03 -0800

    added simple producer

--- source/input-handling/consumer.py
+++ source/input-handling/consumer.py
@@ -2,7 +2,16 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql import SQLContext
+from kafka import KafkaProducer
 # import psycopg2
+
+PRODUCER = KafkaProducer(bootstrap_servers='10.0.0.12:9092, 10.0.0.8:9092, 10.0.0.7:9092')
+
+def handler(message):
+    records = message.collect()
+    for record in records:
+        PRODUCER.send('spark_out', str(record))
+        PRODUCER.flush()
 
 def word_counts(kafka_stream):
     lines = kafka_stream.map(lambda x: x[1])
@@ -11,6 +20,9 @@ def word_counts(kafka_stream):
         .reduceByKey(lambda a, b: a+b)
     return counts
 
+def key_strokes(kafka_stream):
+    '''get bits of keystroke and timestamp into digraphs'''
+    pass
 
 sparkContext = SparkContext(appName = 'testJob')
 sparkContext.setLogLevel('ERROR')
@@ -31,6 +43,9 @@ counts = word_counts(kafkaStream)
 #     .map(lambda word: (word, 1)) \
 #     .reduceByKey(lambda a, b: a+b)
 counts.pprint()
+handler(counts)
+
+
 
 sparkStreamingContext.start()
 sparkStreamingContext.awaitTermination()
