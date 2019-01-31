783b0c5544d1ccd3c8ced8708b84d2cb0bd20d73
jacktavitt <jacktavitt@gmail.com>
2019-01-31 13:05:31 -0800

    put correct stream name

--- source/input-handling/consumer.py
+++ source/input-handling/consumer.py
@@ -11,7 +11,8 @@ sparkStreamingContext = StreamingContext(sparkContext, 1)
 # ['34.215.198.60:9092','34.217.16.2:9092','18.236.99.206:9092']
 kafkaStream = KafkaUtils.createDirectStream(sparkStreamingContext,
             ['hot_topic'],
-            {'metadata.broker.list':'34.215.198.60:9092, 34.217.16.2:9092, 18.236.99.206:9092'})
+             {'metadata.broker.list':'10.0.0.12:9092, 10.0.0.8:9092, 10.0.0.7:9092'})
+            # {'metadata.broker.list':'34.215.198.60:9092, 34.217.16.2:9092, 18.236.99.206:9092'})
 
 # kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
 lines = kafkaStream.map(lambda x: x[1])
