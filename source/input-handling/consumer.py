11b1ebe8a78a1768dffbc8cf3cd0a67444408443
jacktavitt <jacktavitt@gmail.com>
2019-01-31 12:50:16 -0800

    un-listed brokers

--- source/input-handling/consumer.py
+++ source/input-handling/consumer.py
@@ -11,9 +11,7 @@ sparkStreamingContext = StreamingContext(sparkContext, 1)
 # ['34.215.198.60:9092','34.217.16.2:9092','18.236.99.206:9092']
 kafkaStream = KafkaUtils.createDirectStream(sparkStreamingContext,
             ['hot_topic'],
-            {'metadata.broker.list':['34.215.198.60:9092',
-                                    '34.217.16.2:9092',
-                                    '18.236.99.206:9092']})
+            {'metadata.broker.list':'34.215.198.60:9092, 34.217.16.2:9092, 18.236.99.206:9092'})
 
 # kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
 lines = kafkaStream.map(lambda x: x[1])
