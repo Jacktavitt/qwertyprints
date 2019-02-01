98c0ea5adf9edeca12a7a923d8a366a783c89098
jacktavitt <jacktavitt@gmail.com>
2019-01-31 23:44:21 -0800

    plain-jane python implementation of LightGBM

--- source/ml-hosting/simple_connections.py
+++ source/ml-hosting/simple_connections.py
@@ -1,22 +1,30 @@
 import pyspark
 import json
+import numpy as np
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 from pyspark import SparkContext, SparkConf
-from pyspark.sql.functions import lit
+from collections import defaultdict
 import pymongo
 from pymongo import MongoClient
+import lightgbm as lgb
 
-# TODO: run this once for each user initially, then once each time new user data is entered.
+
+# Parameters for training
+param = {'num_leaves':31, 'num_trees':200, 'objective':'binary',
+         'metric': 'auc'}
+# number of boosting iterations
+num_round = 100
+
+# create Mongo connection
+client = MongoClient("mongodb://ec2-52-40-193-219.us-west-2.compute.amazonaws.com:27017/test")
+db = client['test']
+collection = db['mvpModels']
 
 conf = SparkConf().setAppName('dbreadtest').setMaster("local[*]")
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
-user_id = 75
 default_user_model = "~/qwertyprints/data/user_models.user075_model.json"
-
-# structured_query = "(SELECT * FROM mvp_schema WHERE user_id = '001' LIMIT 1000) foo"
-# userdf = spark.sql(structured_query)
 
 whole_df = spark.read \
     .format("jdbc") \
@@ -28,43 +36,37 @@ whole_df = spark.read \
     .load()
 # how many distinct keypairs?
 whole_df.select(whole_df["key_pair"]).distinct().count()
-# user's info'
-user_info = whole_df.filter(whole_df['user_id'] == user_id)
-# comparison info
-# TODO: only grab those values that have the same key_pairs as the user 
-# kps = df.select(df['key_pair']).distinct().collect()
-# other_users_info = whole_df.filter(whole_df['user_id'] != user_id).filter(whole_df['key_pair'] in kps)
-other_users_info = whole_df.filter(whole_df['user_id'] != user_id)
-
-# Now i've got one user's data and data from other users.
-
-# I think i need to label data such that it is true of it is user or false otherwise
-label_df = whole_df.withColumn("label", lit(whole_df['user_id'] == user_id))
 
 # TRainiNG MaGick!
-# TODO: make a Gradient boost algo here that makes an output
-# pivot table so that the columns are keypairs
-# make another table that has trye/ false values as a mirror
+pivoted = whole_df.groupBy("user_id", "task_id", "session_id").pivot("key_pair").avg("digraph_time")
+feature_df = pivoted.toPandas()
+feature_df_users = feature_df.drop(['user_id', 'session_id', 'task_id'], axis=1)
 
-piv3 = whole_df.groupBy("user_id", "task_id", "session_id").pivot("key_pair").avg("digraph_time")
+lgbm_eval_dict = defaultdict(list)
+all_users = np.arange(1,76)
+for user in all_users:
+    column_names = list(feature_df_users.columns)
 
-# make another table that has trye/ false values as a mirror
+    temp_df = feature_df[feature_df['session_id'].isin([0,1])]
+    train_label = np.array((temp_df['user_id'] == user).values, dtype=int)
+    train_data_matrix = temp_df.drop(['user_id', 'session_id', 'task_id'], axis=1).as_matrix()
+    train_data = lgb.Dataset(train_data_matrix, label=train_label, feature_name=column_names)
 
+    temp_df = feature_df[feature_df['session_id'] == 2]
+    test_label = np.array((temp_df['user_id'] == user).values, dtype=int)
+    test_data_matrix = temp_df.drop(['user_id', 'session_id', 'task_id'], axis=1).as_matrix())
+    test_data = lgb.Dataset(test_data_matrix, label=test_label, feature_name=column_names)
 
-# for now use  JSON file from ../../data/user_models/user075_model.json
-with open(default_user_model) as jf:
-    user_model = json.load(jf)
-# set id so that we can retreive it later
-user_model['_id'] = user_id
-# create Mongo connection
-client = MongoClient("mongodb://ec2-52-40-193-219.us-west-2.compute.amazonaws.com:27017/test")
-db = client['test']
-collection = db['mvpModels']
-# insert it into db
-submit_id = collection.insert_one(user_model).inserted_id
-# maybe assert that submit_id == user_id ?
+    bst = lgb.train(param, train_data, num_round)
+    user_model = bst.dump_model()
 
+    user_model['_id'] = user_id
 
+    # insert it into db
+    submit_id = collection.insert_one(user_model).inserted_id
+    # maybe assert that submit_id == user_id ?
+
+print("done.\n")
 
 
 
