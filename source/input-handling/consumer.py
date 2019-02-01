21097c8edafadc0049529300328a97889e7ca02c
jacktavitt <jacktavitt@gmail.com>
2019-01-31 21:58:22 -0800

    added user input synthesizer

--- source/input-handling/consumer.py
+++ source/input-handling/consumer.py
@@ -24,30 +24,19 @@ def key_strokes(kafka_stream):
     '''get bits of keystroke and timestamp into digraphs'''
     pass
 
-sparkContext = SparkContext(appName = 'testJob')
-sparkContext.setLogLevel('ERROR')
-sparkStreamingContext = StreamingContext(sparkContext, 3)
-
-
-
-# ['34.215.198.60:9092','34.217.16.2:9092','18.236.99.206:9092']
-kafkaStream = KafkaUtils.createDirectStream(sparkStreamingContext,
+def main():
+    sparkContext = SparkContext(appName = 'testJob')
+    sparkContext.setLogLevel('ERROR')
+    sparkStreamingContext = StreamingContext(sparkContext, 3)
+    kafkaStream = KafkaUtils.createDirectStream(sparkStreamingContext,
             ['hot-topic'],
-             {'metadata.broker.list':'10.0.0.12:9092, 10.0.0.8:9092, 10.0.0.7:9092'})
-            # {'metadata.broker.list':'34.215.198.60:9092, 34.217.16.2:9092, 18.236.99.206:9092'})
+            {'metadata.broker.list':'10.0.0.12:9092, 10.0.0.8:9092, 10.0.0.7:9092'})
+    counts = word_counts(kafkaStream)
+    counts.pprint()
+    counts.foreachRDD(handler)
+    
+    sparkStreamingContext.start()
+    sparkStreamingContext.awaitTermination()
 
-# kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-counts = word_counts(kafkaStream)
-# lines = kafkaStream.map(lambda x: x[1])
-# counts = lines.flatMap(lambda line: line.split(" ")) \
-#     .map(lambda word: (word, 1)) \
-#     .reduceByKey(lambda a, b: a+b)
-counts.pprint()
-counts.foreachRDD(handler)
-
-
-
-sparkStreamingContext.start()
-sparkStreamingContext.awaitTermination()
-
-
+if __name__=="__main__":
+    main()
--- source/input-handling/synth_user.py
+++ source/input-handling/synth_user.py
@@ -2,10 +2,7 @@ from pyspark.sql import SQLContext
 from kafka import KafkaProducer
 from time import sleep
 import argparse
-import path
-
-# sample filename: /home/johnny/Documents/INSIGHT/project/keystrokes/U_of_Buffalo/UB_keystroke_dataset/s0/baseline/002001.txt
-PRODUCER = KafkaProducer(bootstrap_servers=['34.215.198.60:9092','34.217.16.2:9092','18.236.99.206:9092'])
+from os import path
 
 def split_file_name(file_name):
     user_id = int(file_name[:3])
@@ -14,14 +11,18 @@ def split_file_name(file_name):
     return user_id, session_id, task_id
 
 def main(filename, PRODUCER):
-    # open file of information
+    '''
+    pretends to be a user typing, based on raw data from the university of buffalo. Sends over
+    a Kafka topic 'synth_user'
+    '''
     with open(filename) as f:
         info = f.read()
-    user_id, session_id, _ = split_file_name(filename)
+    user_id, session_id, _ = split_file_name(path.split(filename)[1])
     splitinfo = [x.split(' ') for x in info.split('\n')]
     base_time = int(splitinfo[0][-1])
     time_list = []
-    for index, line in enumerate(splitinfo):
+
+    for line in splitinfo:
         if len(line) == 3 and line[1].lower() == 'keydown':
             tl = [user_id, session_id, line[0], line[1], int(line[2]) - base_time]
             time_list.append(tl)
@@ -32,7 +33,9 @@ def main(filename, PRODUCER):
         PRODUCER.send('synth_user', bytes(str(entry), 'utf-8'))
         
 if __name__=="__main__":
-    args = argparse.ArgumentParser()
-    args.add_argument('-f', '--filename', required=True,
-        help='file of keystrokes to open')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', required=True,
+        help='file of keystrokes to open, sample filename: /home/johnny/Documents/INSIGHT/project/keystrokes/U_of_Buffalo/UB_keystroke_dataset/s0/baseline/002001.txt')
+    args = parser.parse_args()
     PRODUCER = KafkaProducer(bootstrap_servers=['34.215.198.60:9092','34.217.16.2:9092','18.236.99.206:9092'])
+    main(args.filename, PRODUCER)
