1853564d750f1afecd65dc06ed83ebde7c34396b
jacktavitt <jacktavitt@gmail.com>
2019-02-04 23:13:04 -0800

    working on frontend configuration

--- source/front-end/simple_ui.py
+++ source/front-end/simple_ui.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello world"
+
+@app.route('/user/<int:username>')
+def get_user_auth(username):
+    return f"hello {username}"
