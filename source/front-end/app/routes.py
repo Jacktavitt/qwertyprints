f7ce92fe2177fa7502379eed9be248af6eb71d5e
jacktavitt <jacktavitt@gmail.com>
2019-02-05 11:08:30 -0800

    server incorporating js

--- source/front-end/app/routes.py
+++ source/front-end/app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+# from flask import render_template
+from flask import Flask, render_template, request, redirect, Response
+import random, json
 from app import app
 
 @app.route('/')
@@ -6,3 +8,10 @@ from app import app
 @app.route('/index/<int:userid>')
 def index(userid):
     return render_template('index.html', title="Home", user=userid)
+
+@app.route('/receiver', methods = ['POST'])
+def worker():
+    data = request.get_json()
+    print(str(data))
+
+    return str(data)
