c2a6b14bf4b96e09f733d645265700f1a243d2e7
jacktavitt <jacktavitt@gmail.com>
2019-02-05 11:23:21 -0800

    kafka producer in flask app

--- source/front-end/app/routes.py
+++ source/front-end/app/routes.py
@@ -2,6 +2,10 @@
 from flask import Flask, render_template, request, redirect, Response
 import random, json
 from app import app
+from kafka import KafkaProducer
+
+PRODUCER = KafkaProducer( \
+    bootstrap_servers=['34.215.198.60:9092','34.217.16.2:9092','18.236.99.206:9092'])
 
 @app.route('/')
 
@@ -13,5 +17,5 @@ def index(userid):
 def worker():
     data = request.get_json()
     print(str(data))
-
+    PRODUCER.send('user_input', bytes(str(data), 'utf-8'))
     return str(data)
