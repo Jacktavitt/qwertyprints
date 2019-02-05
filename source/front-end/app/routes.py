fbe0a1b00328221092cc7d6d8cf76ac3c5b57ff9
jacktavitt <jacktavitt@gmail.com>
2019-02-05 13:20:26 -0800

    putting producer in flask

--- source/front-end/app/routes.py
+++ source/front-end/app/routes.py
@@ -8,6 +8,8 @@ PRODUCER = KafkaProducer( \
     bootstrap_servers=['54.218.73.149:9092','50.112.197.74:9092','34.222.135.111:9092'])
 
 @app.route('/')
+def output():
+    return render_template('index.html', title="Home")
 
 @app.route('/index/<int:userid>')
 def index(userid):
@@ -17,7 +19,18 @@ def index(userid):
 def worker():
     data = request.get_json()
     res = [str(item) for item in data]
-
     print(res)
     PRODUCER.send('user_input', bytes(res, 'utf-8'))
     return res
+
+@app.route('/receiver', methods = ['POST'])
+def worker():
+	# read json + reply
+	data = request.get_json()
+	result = ''
+
+	for item in data:
+		# loop over every row
+		result += str(item['make']) + '\n'
+    PRODUCER.send('user_input', bytes(result, 'utf-8'))
+	return result
