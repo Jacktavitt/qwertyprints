469d40ef2d6469f32111ed66005584e161ae8484
jacktavitt <jacktavitt@gmail.com>
2019-02-06 01:15:59 -0800

    building up evaluator

--- source/ml-hosting/evaluate_model.py
+++ source/ml-hosting/evaluate_model.py
@@ -1,11 +1,9 @@
 
 import json
-# import lightgbm as lgb
+import lightgbm as lgb
 import pandas as pd
 import numpy as np
-import pymongo
 from pprint import pprint
-from pymongo import MongoClient
 from pyspark.sql import Row, SparkSession
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -14,19 +12,17 @@ from pyspark.sql import SQLContext
 from kafka import KafkaProducer
 from pyspark.sql.types import *
 from pyspark.sql.functions import split, trim
+import DataSculpting
 
 def getSparkSessionInstance(sparkConf):
     if ('sparkSessionSingletonInstance' not in globals()):
-        globals()['sparkSessionSingletonInstance'] = SparkSession\
-            .builder\
-            .config(conf=sparkConf)\
-            .config("spark.mongodb.input.uri", "mongodb://ec2-52-40-193-219.us-west-2.compute.amazonaws.com:27017/models.keystrokes")\
-            .config("spark.mongodb.output.uri", "mongodb://ec2-52-40-193-219.us-west-2.compute.amazonaws.com:27017/models.keystrokes")\
+        globals()['sparkSessionSingletonInstance'] = SparkSession \
+            .builder \
+            .config(conf=sparkConf) \
             .getOrCreate()
     return globals()['sparkSessionSingletonInstance']
 
-def form_ml_shape(kafka_stream):
-    # TODO: must somehow split this into separate rows!
+def lines_from_stream(kafka_stream):
     lines = kafka_stream.map(lambda x: x[1])
     return lines
 
@@ -34,18 +30,15 @@ if __name__ == "__main__":
     sparkContext = SparkContext(appName = 'evaluateModels')
     sparkContext.setLogLevel('ERROR')
     sparkStreamingContext = StreamingContext(sparkContext, 3)
-
     spark = getSparkSessionInstance(sparkContext.getConf())
-    # model_store = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
-    # model_store.printSchema()
 
     kafkaStream = KafkaUtils.createDirectStream(sparkStreamingContext,
             ['user_input'],
             {'metadata.broker.list':'10.0.0.12:9092, 10.0.0.8:9092, 10.0.0.7:9092'})
 
-    keys = form_ml_shape(kafkaStream)
+    keys = lines_from_stream(kafkaStream)
 
-    def process(rdd):
+    def model_user_input(rdd):
 
         try:
             spark=getSparkSessionInstance(rdd.context.getConf())
@@ -68,25 +61,23 @@ if __name__ == "__main__":
             users = typdf.select('user_id').distinct().rdd.flatMap(lambda x: x).collect()
             for user in users:
                 temp = typdf.filter(typdf['user_id']==user)
-                temp.show()
-                # print(type(user), user)
-                pipeline = "{{'$match': {{'_id': {}}}}}".format(user)
-                user_model = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
-                        .option("pipeline", pipeline).load()
-                print("loaded user mdoel")
-                user_model.printSchema()
-            # wordsDataFrame = spark.createDataFrame(rowRdd)
-            # wordsDataFrame.show()
-            # here we do the pivot into usedul feature matrix with pandas
-            # load and evaluate the model with lgbm
-            # if it passes, send result
+                # temp.show()
+                # TODO: Load user models and see if it can make sense
+                # print("loaded user model")
+                # user_model.printSchema()
 
-            # bst = lgb.Booster(model_file='model.txt')
-            # ypred = bst.predict(test_data_mat)`
+                # here we do the pivot into usedul feature matrix 
+                # make it into pandas
+                # load and evaluate the model with lgbm
+                bst = lgb.Booster(model_file='model.txt')
+                ypred = bst.predict(test_data_mat)
+                # if it passes, send result
+
+               
         except Exception as e:
             print(e)
 
-    keys.foreachRDD(process)
+    keys.foreachRDD(model_user_input)
     sparkStreamingContext.start()
     sparkStreamingContext.awaitTermination()
 
