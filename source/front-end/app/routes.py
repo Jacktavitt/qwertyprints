3369bf4d787a5b9f75f3d83004a25295ad143d6b
jacktavitt <jacktavitt@gmail.com>
2019-02-08 01:59:20 -0800

    consuming messages from evaluation

--- source/front-end/app/routes.py
+++ source/front-end/app/routes.py
@@ -1,8 +1,8 @@
 
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaConsumer
+bs = ['54.218.73.149:9092','50.112.197.74:9092','34.222.135.111:9092']
+PRODUCER = KafkaProducer(bootstrap_servers=bs)
 
-PRODUCER = KafkaProducer( \
-     bootstrap_servers=['54.218.73.149:9092','50.112.197.74:9092','34.222.135.111:9092'])
 
 import sys
 from app import app
@@ -15,18 +15,18 @@ def home():
 # serve index template
     return render_template('keylog.html')
 
-# @app.route('/<int:user>')
-# def output(user):
-# # serve index template
-#     return render_template('keylog.html')
 
 @app.route('/<int:user>/receiver', methods = ['POST'])
 def worker(user):
     # read json + reply
+
     data = request.get_json()
     if data:
         message = '|'.join([f"{user},{user},{dig['k']},{dig['t']}" for dig in data['value']]).replace(' ','Space')
         PRODUCER.send('user_input', bytes(message, 'utf-8'))
-        return message
     else:
-        return "FALSE"
+        message = "False"
+
+    consumer = KafkaConsumer('user{}_sess{}'.format(user,user), bootstrap_servers=bs)
+
+    return consumer
