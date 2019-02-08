75ac7844f2e94985b1db87ac950127d81228a297
jacktavitt <jacktavitt@gmail.com>
2019-02-08 12:16:47 -0800

    trying to make response active

--- source/front-end/app/routes.py
+++ source/front-end/app/routes.py
@@ -1,8 +1,9 @@
 
 from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaClient, SimpleConsumer
 bs = ['54.218.73.149:9092','50.112.197.74:9092','34.222.135.111:9092']
 PRODUCER = KafkaProducer(bootstrap_servers=bs)
-
+CLIENT = KafkaClient(bs)
 
 import sys
 from app import app
@@ -30,19 +31,18 @@ def worker(user):
 
 @app.route('/<int:user>/auth')
 def authentication(user):
-    consumer = KafkaConsumer('user{}_sess{}'.format(user,user), bootstrap_servers=bs)
-    # return Response(kafkastream(consumer), mimetype='text/xml')
-    def kafkastream():
-        for msg in consumer:
-            yield('''<html>
-        <head>
-            <title>Am I Authenticating</title>
-        </head>
-        <body>
-            <h1>Message is: ''' + msg.value.decode() + '''</h1>
-        </body>
-    </html>msg.value)''')
-    return Response(kafkastream(), mimetype='text/xml')
+    # consumer = KafkaConsumer('user{}_sess{}'.format(user,user), bootstrap_servers=bs)
+    consumer = SimpleConsumer(CLIENT, 'testing', 'user{}_sess{}'.format(user,user))
+    # res = consumer.poll(timeout_ms=6000)
+    msg = consumer.get_message()
+    color='yellow'
+    if msg:
+        if msg.message.value.decode() == 'True':
+            color='green'
+        else:
+            color='red'
+    render_template('auth_result.html', bgcolor=color)
+    # return Response(kafkastream(), mimetype='text/xml')
 
 
 # def kafkastream(consumer):
