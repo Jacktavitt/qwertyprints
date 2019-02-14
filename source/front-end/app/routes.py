713b774c7664bdca83427ea8ddd0853ec7b37918
jacktavitt <jacktavitt@gmail.com>
2019-02-13 17:06:28 -0800

    timing delay for both frontend and spark

--- source/front-end/app/routes.py
+++ source/front-end/app/routes.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template, request, redirect, Response
 import random, json
 
 SEND_TIME = None
+print("At top of function \n\n")
 RECEIVE_TIME = None
 
 bs = ['54.218.73.149:9092','50.112.197.74:9092','34.222.135.111:9092']
