d3fab81f238d0b2671912cd51e689f07fcc4bf5a
jacktavitt <jacktavitt@gmail.com>
2019-02-15 13:07:36 -0800

    adding more logging strings to evalation for timing

--- source/ml-hosting/evaluate_model.py
+++ source/ml-hosting/evaluate_model.py
@@ -56,6 +56,7 @@ def main():
     # boto_client = boto3.client('s3')
     bucket_name = 'user-ml-models'
     PRODUCER = KafkaProducer(bootstrap_servers=['54.218.73.149:9092','50.112.197.74:9092','34.222.135.111:9092'])
+    _STREAM_START = time.time()
     kafkaStream = KafkaUtils.createDirectStream(sparkStreamingContext,
             ['user_input'],
             {'metadata.broker.list':'10.0.0.12:9092, 10.0.0.8:9092, 10.0.0.7:9092'})
@@ -64,9 +65,9 @@ def main():
     def model_user_input(rdd):
 
         try:
-            _start_spark = time.time()
+            _SPARK_START = time.time()
             spark=getSparkSessionInstance(rdd.context.getConf())
-
+            _INPUT_CHOP_START = time.time()
              # Convert RDD[String] to RDD[Row] to DataFrame
             rows = rdd.flatMap(lambda line: line.split('|'))
             rdf = spark.createDataFrame(rows, StringType())
@@ -83,8 +84,11 @@ def main():
             winder = Window.partitionBy("user_id").orderBy("action_time")
 
             users = typdf.select('user_id').distinct().rdd.flatMap(lambda x: x).collect()
+            _INPUT_CHOP_TIME = time.time() - _INPUT_CHOP_START
+            _USER_LOOP_START = time.time()
             # users.remove(None)
             for user in users:
+                _USERCHOP_START = time.time()
                 temp = typdf.filter(typdf['user_id']==user)
                 sessions = temp.select('session_id').distinct().rdd.flatMap(lambda x: x).collect()
                 timed = temp.withColumn("digraph_time", (temp["action_time"] - lag(temp["action_time"], 1).over(winder)))
@@ -96,8 +100,12 @@ def main():
                 pivoted = model_data.groupBy("user_id", "session_id") \
                         .pivot("key_pair") \
                         .avg("digraph_time")
+                _USERCHOP_TIME = time.time() - _USERCHOP_START
+                _USERPANDA_START = time.time()
                 feature_df = pivoted.toPandas()
+                _USERPANDA_TIME = time.time() - _USERPANDA_START
                 the_data_matrix = feature_df.drop(['user_id', 'session_id'], axis=1).as_matrix()
+                _FILEIO_START = time.time()
                 if not os.path.isfile('{}.txt'.format(user)):
                     s3_obj = s3.Object(bucket_name, "{}.json".format(user))
                     try:
@@ -107,7 +115,8 @@ def main():
                     text_model = user_model['model']
                     with open('{}.txt'.format(user), 'w+') as fw:
                         fw.write(text_model)
-
+                _FILEIO_TIME = time.time() - _FILEIO_START
+                _MODEL_START = time.time()
                 bst = lgb.Booster(model_file='{}.txt'.format(user))
                 # now evaluate
                 ypred = bst.predict(the_data_matrix)
@@ -115,8 +124,11 @@ def main():
                 # result = "{}{}".format(str(translate_prediction_value(ypred))[:4], time.time())
                 result = "{}".format(calib_pred > 0.5)
                 # for sess in sessions: 
+                _MODEL_TIME = time.time() - _MODEL_START
                 PRODUCER.send('user{}_sess{}'.format(user, user), bytes(str(result), 'utf-8'))
                 print("user: {} ypred: {} calib_pred: {} result: {} delay: {}".format(user, ypred[0], calib_pred, result, (time.time()-_start_spark)))
+                print("\n_INPUT_CHOP_TIME: {}\_USERCHOP_TIME: {}\n\_USERPANDA_TIME: {}\n_FILEIO_TIME: {}\n_MODEL_TIME: {}\n" \
+                        .format(_INPUT_CHOP_TIME, _USERCHOP_TIME,_USERPANDA_TIME, _FILEIO_TIME, _MODEL_TIME))
 
         except Exception as e:
             print(e)
